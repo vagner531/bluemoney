70571e05ce50e405d47c6370c817d8f398909b8d
vagner santos <vagner131417@gmail.com>
2022-11-01 23:27:33 -0300

    transactions

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,16 @@
+import React, { useContext } from 'react';
 import iconImg from '../../assets/entrada.svg'
 import outcomeImg from '../../assets/saida.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
+
+  console.log(transactions);
+  
   return (
     <Container>
       <div>
