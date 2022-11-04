55a51c8a78ffc34ac7e75e3f4a866bac894e6398
vagner santos <vagner131417@gmail.com>
2022-11-03 22:22:47 -0300

    inset infos

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,7 +10,7 @@ interface Transaction {
   createdAt: string;
 }
 
-type TransactionInput = Omit<Transaction, '1' | 'createdAt'>;
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 interface TransactionsProviderProps {
   children: ReactNode;
@@ -18,7 +18,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -34,8 +34,18 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ]);
+
   }
 
   return (
